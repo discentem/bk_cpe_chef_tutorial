d9553cc044eb4d9ad74350152657a8595808dde7
BK <brandonk@uber.com>
2025-02-04 16:51:56 -0500

    add fb_launchd

---
+++ cookbooks/fb_launchd/metadata.rb
@@ -0,0 +1,10 @@
+# Copyright (c) 2018-present, Facebook, Inc.
+name 'fb_launchd'
+maintainer 'Facebook'
+maintainer_email 'noreply@facebook.com'
+license 'Apache-2.0'
+description 'Installs/Configures Chef-defined launchd plists'
+source_url 'https://github.com/facebook/chef-cookbooks/'
+version '0.1.0'
+supports 'mac_os_x'
+depends 'fb_helpers'
